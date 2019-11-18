81706036be1c206c2b434d6c5d50d494b760389b
sony <sony@localhost.localdomain>
2019-11-18 08:28:13 +0000

    cleaning

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { observable, computed, action, decorate, runInAction } from "mobx";
-import { get, set, entries, remove } from "mobx";
+import { entries } from "mobx";
 import * as firebaseService from "./firebaseService";
 
 export class Store {
@@ -7,7 +7,6 @@ export class Store {
     this.activeUser = null;
     this.loading = false;
     this.authCheckComplete = false;
-    this.items = new Map();
     this.initializationError = null;
 
     this.initializeStore().then(u => {
@@ -16,12 +15,6 @@ export class Store {
     });
   }
 
-  /**
-   * if we have an authenticated user then get all of the profile
-   * information from the database and associate it with the active
-   * user
-   * @param {*} _authUser
-   */
   handleAuthedUser = async _authUser => {
     if (_authUser) {
       let userAcctInfo = await firebaseService.getUserProfile();
@@ -56,31 +49,11 @@ export class Store {
       return null;
     }
   }
-  /**
-   * here we check to see if ionic saved a user for us
-   */
+
   get authenticatedUser() {
     return this.activeUser || null;
   }
 
-  /**
-   * gets all of the items as an array from the map
-   */
-  get itemEntries() {
-    return entries(this.items);
-  }
-
-  /**
-   * get a specific item based on its key
-   * @param {*} _key
-   */
-  itemByKey(_key) {
-    return get(this.items, _key);
-  }
-
-  /**
-   * login using a username and password
-   */
   doLogin(_username, _password) {
     debugger;
     if (_username.length) {
@@ -102,109 +75,11 @@ export class Store {
     }
   }
 
-  /**
-   * create the user with the information and set the user object
-   */
-  async doCreateUser(_params) {
-    try {
-      let newUser = await firebaseService.registerUser({
-        email: _params.email,
-        password: _params.password,
-        firstName: _params.firstName,
-        lastName: _params.lastName
-      });
-      return newUser;
-    } catch (err) {
-      debugger;
-      console.log(err);
-      return err;
-      // for (let e of err.details) {
-      //   if (e === "conflict_email") {
-      //     alert("Email already exists.");
-      //   } else {
-      //     // handle other errors
-      //   }
-      // }
-    }
-  }
-
-  /**
-   * logout and remove the user...
-   */
   doLogout() {
     this.activeUser = null;
     return firebaseService.logOut();
   }
-
-  // DATA CRUD
-  loadData() {
-    return firebaseService
-      .queryObjectCollection({ collection: "items" })
-      .then(
-        _result => {
-          // create the user object based on the data retrieved...
-          return runInAction(() => {
-            let resultMap = _result.reduce((map, obj) => {
-              map[obj.id] = obj;
-              return map;
-            }, {});
-            this.items = resultMap;
-            return resultMap;
-          });
-        },
-        err => {
-          console.log(err);
-          return err;
-        }
-      )
-      .catch(e => {
-        console.log(e);
-        return e;
-      });
-  }
-  addItem(_data) {
-    return firebaseService
-      .addObjectToCollection({ collection: "items", objectData: _data })
-      .then(
-        _result => {
-          // create the user object based on the data retrieved...
-          return runInAction(() => {
-            set(this.items, _result.id, _result);
-            return _result;
-          });
-        },
-        err => {
-          console.log(err);
-          return err;
-        }
-      )
-      .catch(e => {
-        console.log(e);
-        return e;
-      });
-  }
-
-  deleteItem(_data) {
-    return firebaseService
-      .removeObjectFromCollection({ collection: "items", objectId: _data.id })
-      .then(
-        _result => {
-          // create the user object based on the data retrieved...
-          return runInAction(() => {
-            remove(this.items, _data.id);
-            return true;
-          });
-        },
-        err => {
-          console.log(err);
-          return err;
-        }
-      )
-      .catch(e => {
-        console.log(e);
-        return e;
-      });
-  }
+  
 }
 
 decorate(Store, {
@@ -212,20 +87,14 @@ decorate(Store, {
   activeUser: observable,
   loading: observable,
   authCheckComplete: observable,
-  items: observable,
   initializationError: observable,
 
   // COMPUTED
   authenticatedUser: computed,
   doCheckAuth: computed,
-  itemEntries: computed,
 
   // ACTIONS
-  doCreateUser: action,
   doLogin: action,
   doLogout: action,
-  loadData: action,
-  itemByKey: action,
-  addItem: action,
-  deleteItem: action
+  
 });
