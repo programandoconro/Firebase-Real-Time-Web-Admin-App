fb318c8c757008a331ddd1b6a4c1687a88906fb8
sony <sony@localhost.localdomain>
2019-11-21 15:18:48 +0000

    gitignore

--- node_modules/@ionic/react/dist/types/components/navigation/routing/ReactRouter/IonReactRouter.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { BrowserRouterProps } from 'react-router-dom';
-export declare class IonReactRouter extends React.Component<BrowserRouterProps> {
-    render(): JSX.Element;
-}
