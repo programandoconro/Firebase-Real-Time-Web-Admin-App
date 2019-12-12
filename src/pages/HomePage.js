c5e108131c1d03a0d3ca18e3d9bb38747032fcd2
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-12 17:06:31 +0000

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,21 +6,21 @@ import {
   IonToolbar,
   IonButton,
   IonList,
-
+  
 } from "@ionic/react";
 import TabContainer from "../components/TabContainer";
-import firebase from '@firebase/app'
+import firebase from '@firebase/app';
 
 const writeAdminData =(userInfo)=> {
   firebase.database().ref('user0001').push({
-      userInfo
+    userInfo
   }).catch((error)=>{
-      console.log('error ' , error)
+    console.log('error ' , error);
   })
 }
 
 class HomePage extends Component {
-
+  
   constructor() {
     super();
     this.state = {
@@ -28,22 +28,22 @@ class HomePage extends Component {
       data:[],
       reservas:[],
       value:  [],
-
+      
     };
-  }
-
+  };
+  
   componentWillUpdate () {
-
-      const readUsersData = ()=> {
+    
+    const readUsersData = ()=> {
       const nameRef =  firebase.database().ref('user0001')
       nameRef.on('value', (snapshot)=> {
-        const state = snapshot.val()
-        this.state.reservas =  state
-    })
+        const state = snapshot.val();
+        this.state.reservas =  state;
+      })
     }
-    readUsersData()
-    }
-
+    readUsersData();
+  }
+  
   _changedTabs = e => {
     if (e.currentTarget.attributes.tab.value === "tab1") {
       this.setState(() => ({ onListPage: true }));
@@ -51,68 +51,68 @@ class HomePage extends Component {
       this.setState(() => ({ onListPage: false }));
     }
   }
-
+  
   render() {
-
+    
     const pushAdminData = (data)  => {
       this.setState({data })
     }
-
+    
     const reservas =  JSON.stringify(this.state.reservas)
     const tabla =  reservas.split (',').map ((item, i) => <p key={i}>{item}</p>)
-
+    
     return (
       
       <IonPage>
-        <IonHeader>
-          <IonToolbar color="primary">
-          </IonToolbar>       
-        <h1 style={{flex:1,textAlign:'center', alignContent:'center'}}>Introduzca la reserva a confirmar:</h1>
-         <div             style={myStyle}  
- >
-          <input 
-            onChange={ e=> this.setState({value: e.target.value})}
-            value={this.state.value} 
-             
-           > 
-          </input>  
-</div>
-
-<div             style={myStyle}  
- >
-          <IonButton
-          onClick= {()=>{
-            pushAdminData(this.state.value) ;
-            writeAdminData( JSON.stringify(this.state.value).replace(/["{[,}\]]/g, "") ) ;
-            
-          }}
-          
-         > Enviar </IonButton>
-         </div>           
-         </IonHeader>        
-        
-          <IonContent >
-           <div style={myStyle}> 
-            <IonList    > {tabla.reverse()}  </IonList>   
-            </div>
-          <TabContainer
-            history={this.props.history}
-            changedTabs={e => this._changedTabs(e)}
-            />
-                  
-       </IonContent>
-       
+      <IonHeader>
+      <IonToolbar color="primary">
+      </IonToolbar>       
+      <h1 style={{flex:1,textAlign:'center', alignContent:'center'}}>Introduzca la reserva a confirmar:</h1>
+      <div             style={myStyle}  
+      >
+      <input 
+      onChange={ e=> this.setState({value: e.target.value})}
+      value={this.state.value} 
+      
+      > 
+      </input>  
+      </div>
+      
+      <div             style={myStyle}  
+      >
+      <IonButton
+      onClick= {()=>{
+        pushAdminData(this.state.value) ;
+        writeAdminData( JSON.stringify(this.state.value).replace(/["{[,}\]]/g, "") ) ;
+        alert('La confirmación ha sido enviada ' + this.state.value )
+      }}
+      
+      > Enviar </IonButton>
+      </div>           
+      </IonHeader>        
+      
+      <IonContent >
+      <div style={myStyle}> 
+      <IonList    > {tabla.reverse()}  </IonList>   
+      </div>
+      <TabContainer
+      history={this.props.history}
+      changedTabs={e => this._changedTabs(e)}
+      />
+      
+      </IonContent>
+      
       </IonPage>
       
-    );
+      );
+    }
   }
-}
-
-const myStyle = {
-
-  flex:1,textAlign:'center', alignContent:'center',alignItems:'center'
-
-}
-
-export default HomePage
-
+  
+  const myStyle = {
+    
+    flex:1,textAlign:'center', alignContent:'center',alignItems:'center'
+    
+  };
+  
+  export default HomePage;
+  
