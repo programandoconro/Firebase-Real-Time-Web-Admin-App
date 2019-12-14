9c58d57c06cf1029c564728b7051d6372186044d
Ro <50117686+progamandoconro@users.noreply.github.com>
2019-12-14 22:33:19 +0000

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,80 +1,118 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/app';
-import './App.css'
+import './App.css';
+
+const db = () => firebase.database();
 
 function Dashboard() {
-  const [reservas, handleReservas] = useState([]);
+	const [reservas, handleReservas] = useState([]);
   const [input, handleInput] = useState('');
-  
-  useEffect(()=>{
-    firebase
-    .database()
-    .ref('user0001')
-    .on('value', (e)=> (e)
-    )
-    console.log('Mounted ');
-  }
-  )
-  
-  useEffect(()=>{
-    firebase
-    .database()
-    .ref('user0001')
-    .on('value', handleReservas
-    );
-  },[])
-  
-  const deleteColletion =()=> {
-    firebase
-    .database()
-    .ref('user0001').remove()
-    
-  }
-  
-  const tabla = JSON.stringify(reservas);
+  const [data, handleData] = useState('');
 
-  const mytabla =  tabla.split (',').map ((item, i) => <div>
-  <p key={i}>{item
-    .replace(RegExp(/([.*+?^=!$(){}|[\]\\""])/g)," ")
-    .replace("userInfo","").replace(":","->").replace(":"," ")
-  }</p><p> {item.replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g)," ")
-  .replace("userInfo","").replace(":","->").replace(":"," ")}</p>
-  
-  <button onClick={()=>writeAdminData(item)}> Confirmar </button></div>);
-  
-  const writeAdminData =(userInfo)=> {
-    firebase.database().ref('user0001').push({
-      userInfo
-    }).catch((error)=>{
-      console.log('error ' , error)
-    })
-  }
-  
-  return (
-    <div style={myStyle}>
-    <h1>Click a la reserva a confirmar o confirmar manualmente </h1>
-    <input 
-    value={input}
-    onChange={e=>handleInput(e.target.value)}
-    >
-    </input>
-    <button onClick={()=>writeAdminData(input)}> Confirmar </button>
-    <br></br><br></br>
-    <ul className='myList' > { 
-      mytabla.reverse()
+	useEffect(() => {
+		db();
+		console.log('Mounted ');
+	});
+
+	useEffect(() => {
+		db()
+			.ref('user0001')
+      .on('value', handleReservas);
       
-    }
-    </ul>  
-    <button
-    onClick={()=>deleteColletion()}
-    > Limpiar la lista  </button>
-    </div>
-    )
-  }
-  const myStyle = {
-    flex:1,textAlign:'center', alignContent:'center',alignItems:'center'
-    
-  }
+    db().ref('/').on('value', handleData)
+	}, []);
 
-  export default Dashboard;
+	const deleteColletion = () => {
+		firebase
+			.database()
+			.ref('user0001')
+			.remove();
+	};
   
+  const deleteDB = () => {
+		firebase
+			.database()
+			.ref('/')
+			.remove();
+	};
+  
+
+	const tabla = JSON.stringify(reservas);
+	const mytabla = tabla.split(',').map((item, i) => (
+		
+    <div>
+			<p style={myItem}>
+				{item
+          .slice(25)
+					.replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g), ' ')
+					.replace('userInfo', '')
+					.replace(':', '->')
+					.replace(':', ' ')}
+			</p>
+
+			<button onClick={() => writeAdminData(item)} >
+				Confirmar
+			</button>
+      <button onClick={() => writeAdminData(item)} >
+				Rechazar
+			</button>
+		</div>
+  ));
+
+  const myData = JSON.stringify(data)
+  
+ const myDashboard = myData.split(',').map((item, i) => (
+		
+    <div>
+			<p>
+				{item
+					.replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g), ' ')
+					.replace('userInfo', '')
+					.replace(':', '->')
+					.replace(':', ' ')}
+			</p>
+    
+		</div>
+  ));
+
+	const writeAdminData = (userInfo) => {
+		firebase
+			.database()
+			.ref('user0001')
+			.push({
+				userInfo
+			})
+			.catch((error) => {
+				console.log('error ', error);
+			});
+	};
+
+	return (
+		<div style={myStyle}>
+			<h1>Click a la reserva a confirmar o confirmar manualmente </h1>
+			<input value={input} onChange={(e) => handleInput(e.target.value)}></input>
+			<button onClick={() => writeAdminData(input)}> Confirmar </button>
+			<br></br>
+			<br></br>
+			<ul className="myList"> {mytabla.reverse()}</ul>
+      <button onClick={() => deleteColletion()}> Limpiar reserva </button>
+	    <ul className='myList'> {myDashboard.reverse()}</ul>
+      <button onClick={() => deleteDB()}> Limpiar todas las reservas </button>
+
+  
+  	</div>
+	);
+}
+const myStyle = {
+	flex: 1,
+	textAlign: 'center',
+	alignContent: 'center',
+	alignItems: 'center'
+};
+
+const myItem ={
+   float: 'left', 
+   display: 'inline' 
+}
+
+export default Dashboard;
