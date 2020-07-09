e4cf4cda38bb38bc0c85cb2bf9a14c47ac5a9db8
programandoconro <programandoconro@gmail.com>
2020-07-09 02:08:58 +0200

    Ortogra

---
+++ build/precache-manifest.18763f942f7485b25e1937fb6a54f285.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79ba739aadff31f3e638406153a15f1",
+    "url": "/Firebase-Real-Time-Web-Admin-App/index.html"
+  },
+  {
+    "revision": "afae4b962f4ad060eaa2",
+    "url": "/Firebase-Real-Time-Web-Admin-App/static/css/main.7f6894d8.chunk.css"
+  },
+  {
+    "revision": "7288f08ee4fd6d4f6e23",
+    "url": "/Firebase-Real-Time-Web-Admin-App/static/js/2.f3e6bbe9.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/Firebase-Real-Time-Web-Admin-App/static/js/2.f3e6bbe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afae4b962f4ad060eaa2",
+    "url": "/Firebase-Real-Time-Web-Admin-App/static/js/main.f9e01218.chunk.js"
+  },
+  {
+    "revision": "7aed8523f9a024b376fb",
+    "url": "/Firebase-Real-Time-Web-Admin-App/static/js/runtime-main.dadef984.js"
+  },
+  {
+    "revision": "69a61ffaa7499da617b0f714bcc677d4",
+    "url": "/Firebase-Real-Time-Web-Admin-App/static/media/cactus.69a61ffa.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Firebase-Real-Time-Web-Admin-App/precache-manifest.022c9f28d9127273c460251a2b2c0ccc.js"
+  "/Firebase-Real-Time-Web-Admin-App/precache-manifest.18763f942f7485b25e1937fb6a54f285.js"
 );
 
 self.addEventListener('message', (event) => {
