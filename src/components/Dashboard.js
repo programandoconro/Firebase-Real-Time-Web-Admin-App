9081dea4448fdf8158f04aa77b22163af35dc4d6
Rodrigo Díaz Lupanow <programandoconro@gmail.com>
2021-11-06 15:16:54 +0900

    Format old project

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,196 +1,209 @@
-import React, { useState, useEffect } from 'react';
-import firebase  from 'firebase/app' ;
-import './App.css';
-import { Typography, Paper,  Button, } from '@material-ui/core';
-import logo from '../assets/cactus.png';
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/app";
+import "./App.css";
+import { Typography, Paper, Button } from "@material-ui/core";
+import logo from "../assets/cactus.png";
 
 const db = () => firebase.database();
 
 function Dashboard() {
-	const [reservas, handleReservas] = useState([]);
-	const [input, handleInput] = useState('');
-	const [data, handleData] = useState('');
-	const [userID, handleuserID] = useState('user0001');
+  const [reservas, handleReservas] = useState([]);
+  const [input, handleInput] = useState("");
+  const [data, handleData] = useState("");
+  const [userID, handleuserID] = useState("");
 
-	useEffect(() => {
-		db().ref('/');
-		
-	})
-	
-	useEffect(() => {
-		db().ref('/').on('value', handleReservas);
-		db().ref('/').on('value', handleData)
-	}, [userID]);
-	
-	const deleteColletion = () => {
-		firebase
-		.database()
-		.ref(userID)
-		.remove();
-	};
-	
-	const deleteDB = () => {
-		firebase
-		.database()
-		.ref('/')
-		.remove();
-	};
-	
-	const logOut = () =>{
-		firebase.auth().signOut().then(function() {
-		}, function(error) {
-			console.log(error)
-		});		
-		
-	}
-	
-	const tabla = JSON.stringify(reservas);
-	const mytabla = tabla.split(',').map((item, k) => (
-		
-		<div key={k}>
-		<p style={myItem} >
-		{item
-			.slice(25)
-			.replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g), ' ')
-			.replace('userInfo', '')
-			.replace(':', '->')
-			.replace(':', ' ')}
-			</p>
-			
-			<Button 
-			onClick={() => writeAdminData(item)}
-			 >
-			Confirmar
-			</Button>
-			</div>
-			));
-			
-			const myData = JSON.stringify(data);
-			const myDashboard = myData.split(',').map((item, k) => (
-				
-				<div key={k}>
-				<p >
-				{item
-					.replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g), ' ')
-					.replace('userInfo', '')
-					.replace(':', '->')
-					.replace(':', ' ')}
-					</p>
-					
-					</div>
-					));
-					
-					const writeAdminData = (userInfo) => {
-						firebase
-						.database()
-						.ref('/'+userID)
-						.push({
-							userInfo
-						})
-						.catch((error) => {
-							console.log('error ', error);
-						});
-					};
-					
-					return (
-						<div style={myStyle}>
-						<Paper >
-						<h1>Panel de Administración de Reservaciones</h1>
-						<img src= {logo} alt='cactus'></img>
-						<h1>Restaurante Cactus</h1>
-				        </Paper>
-						
-						<Paper>
-						<Typography> Bandeja de Entrada </Typography>
-						<ul 
-						className='myList'> 
-						{myDashboard.reverse()}
-						</ul>
-						
-						<Button 
-						type="submit"
-						variant="contained"
-						color="primary"
-						onClick={()=>{if (window.confirm('¿Seguro que desea borrar toda la Bandeja de Entrada?')) deleteDB() }}>
-						Borrar
-						</Button>
-						<hr style={mySeparator}></hr>
-						</Paper>
+  useEffect(() => {
+    db().ref("/");
+  });
 
-						<Typography> Bandeja de Salida </Typography>
-						
-						<Paper>
-						<input 
-						value={input} 
-						onChange={(e) => handleInput(e.target.value)}>
-						</input>
-						<br></br><br></br>
+  useEffect(() => {
+    db().ref("/").on("value", handleReservas);
+    db().ref("/").on("value", handleData);
+  }, [userID]);
 
-						<Button 
-						onClick={() => writeAdminData(input)}
-						type="submit"
-						
-						variant="contained"
-						color="primary"
-						> 
-						Enviar
-						</Button>				
-						<br></br><br></br>
-						
-						<input 
-						placeholder='ID Usuario' 
-						value={userID} 
-						onChange={(e) => handleuserID(e.target.value)}>
-						</input>
-						
-						<ul 
-						className="myList"> 
-						{mytabla.reverse()}
-						</ul>
-						
-						<Button 
-						type="submit"
-						variant="contained"
-						color="primary"
-						onClick={()=>{if (window.confirm('¿Seguro que desea borrar la reservación?')) deleteColletion() }}>
-						Borrar 
-						</Button>
-						<hr style={mySeparator}></hr>
-						</Paper>
+  const deleteColletion = () => {
+    firebase.database().ref(userID).remove();
+  };
 
-						<Paper>
-						<Button 
-						type="submit"
-						variant="contained"
-						color="primary"
-						onClick={() => logOut()}>
-						Salir  
-						</Button>
-						</Paper>
+  const deleteDB = () => {
+    firebase.database().ref("/").remove();
+  };
 
-						<h5>* Instrucciones: 1. Identifique las reservaciones en la Bandeja de Entrada. </h5>					
-						<h5> 2. Ingrese el usuario de interés dentro de la Bandeja de Salida. </h5>					
-						<h5> 3. Confirme directamente la reserva con el botón CONFIRMAR y/o escriba un mensaje con la respuesta deseada. </h5>					
-						
-						</div>
-						);
-					}
-					const myStyle = {
-						flex: 1,
-						textAlign: 'center',
-						alignContent: 'center',
-						alignItems: 'center'
-					}
-					
-					const myItem ={
-						float: 'left', 
-						display: 'inline' 
-					}
+  const logOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(
+        function () {},
+        function (error) {
+          console.log(error);
+        }
+      );
+  };
 
-					const mySeparator ={
-						backgroundColor:'pink',
-						color:'white' 
-					}
-					
-					export default Dashboard;
-					
+  const tabla = JSON.stringify(reservas);
+  const mytabla = tabla.split(",").map((item, k) => (
+    <div key={k}>
+      <p style={myItem}>
+        {item
+          .slice(25)
+          .replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g), " ")
+          .replace("userInfo", "")
+          .replace(":", "->")
+          .replace(":", " ")}
+      </p>
+
+      <Button onClick={() => writeAdminData(item)}>Confirmar</Button>
+    </div>
+  ));
+
+  const myData = JSON.stringify(data);
+  const myDashboard = myData.split(",").map((item, k) => (
+    <div key={k}>
+      <p>
+        {item
+          .replace(RegExp(/([.*+?^=!$(){}|[\]/\\""])/g), " ")
+          .replace("userInfo", "")
+          .replace(":", "->")
+          .replace(":", " ")}
+      </p>
+    </div>
+  ));
+
+  const writeAdminData = (userInfo) => {
+    firebase
+      .database()
+      .ref("/" + userID)
+      .push({
+        userInfo,
+      })
+      .catch((error) => {
+        console.log("error ", error);
+      });
+  };
+
+  return (
+    <div style={myStyle}>
+      <Paper>
+        <h1>Panel de Administración de Reservaciones</h1>
+        <img src={logo} alt="cactus"></img>
+        <h1>Restaurante Cactus</h1>
+      </Paper>
+
+      <Paper>
+        <Typography> Bandeja de Entrada </Typography>
+        <ul className="myList">{myDashboard.reverse()}</ul>
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (
+              window.confirm(
+                "¿Seguro que desea borrar toda la Bandeja de Entrada?"
+              )
+            )
+              deleteDB();
+          }}
+        >
+          Borrar
+        </Button>
+        <hr style={mySeparator}></hr>
+      </Paper>
+
+      <Typography> Bandeja de Salida </Typography>
+
+      <Paper>
+        <input
+          value={input}
+          onChange={(e) => handleInput(e.target.value)}
+        ></input>
+        <br></br>
+        <br></br>
+
+        <Button
+          onClick={() => {
+            if (input.length > 0) {
+              writeAdminData(input);
+              alert("Gracias! se ha enviado exitosamente ");
+              handleInput("");
+              handleuserID("");
+            }
+          }}
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Enviar
+        </Button>
+        <br></br>
+        <br></br>
+
+        <input
+          placeholder="ID Usuario"
+          value={userID}
+          onChange={(e) => handleuserID(e.target.value)}
+        ></input>
+
+        <ul className="myList">{mytabla.reverse()}</ul>
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            if (window.confirm("¿Seguro que desea borrar la reservación?"))
+              deleteColletion();
+          }}
+        >
+          Borrar
+        </Button>
+        <hr style={mySeparator}></hr>
+      </Paper>
+
+      <Paper>
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          onClick={() => logOut()}
+        >
+          Salir
+        </Button>
+      </Paper>
+
+      <h5>
+        * Instrucciones: 1. Identifique las reservaciones en la Bandeja de
+        Entrada.{" "}
+      </h5>
+      <h5>
+        {" "}
+        2. Ingrese el usuario de interés dentro de la Bandeja de Salida.{" "}
+      </h5>
+      <h5>
+        {" "}
+        3. Confirme directamente la reserva con el botón CONFIRMAR y/o escriba
+        un mensaje con la respuesta deseada.{" "}
+      </h5>
+    </div>
+  );
+}
+const myStyle = {
+  flex: 1,
+  textAlign: "center",
+  alignContent: "center",
+  alignItems: "center",
+};
+
+const myItem = {
+  float: "left",
+  display: "inline",
+};
+
+const mySeparator = {
+  backgroundColor: "pink",
+  color: "white",
+};
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,62 +1,60 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import Login from './components/Login'
-import DashBoard from './components/Dashboard'
-import {BrowserRouter , Switch, Route, Redirect} from 'react-router-dom'
-import './index.css'
-import { firebaseAuth } from '../src/store/constans'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import Login from "./components/Login";
+import DashBoard from "./components/Dashboard";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import "./index.css";
+import { firebaseAuth } from "../src/store/firebase_config";
 
-function PrivateRoute ({component: Component, authed, ...rest}) {
-    return (
-        <Route
-        {...rest}
-        render={(props) => authed === true
-            ? <Component {...props} />
-            : <Redirect to={{pathname: '/login', state: {from: props.location}}} />}
-            />
-            )
-        }
-        
-        export default function App() {
-            const [state,handleState] = useState( false );
-            useEffect(()=>{
-                firebaseAuth().onAuthStateChanged((user) => {
-                    if (user) {
-                        handleState(
-                            true
-                            
-                            )
-                        } else {
-                            handleState(
-                                false
-                                
-                                )
-                            }
-                        })                    
-                        
-                    })
-                    
-                    return  (
-                        <BrowserRouter>
-                        <div>
-                        
-                        <div className="container">
-                        <div className="row">
-                        <Switch>
-                        <Route path='/' exact component={Login} />
-                        
-                        <PrivateRoute authed={state} path='/dashboard' component={DashBoard} />
-                        <Route render={() => <Login/>} />
-                        </Switch>
-                        </div>
-                        </div>
-                        </div>
-                        </BrowserRouter>
-                        );
-                    }
-                    
-                    ReactDOM.render(<App />, document.getElementById('root'));
-                    
-                    
-                    
-                    
+function PrivateRoute({ component: Component, authed, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+}
+
+export default function App() {
+  const [state, handleState] = useState(false);
+  useEffect(() => {
+    firebaseAuth().onAuthStateChanged((user) => {
+      if (user) {
+        handleState(true);
+      } else {
+        handleState(false);
+      }
+    });
+  });
+
+  return (
+    <BrowserRouter>
+      <div>
+        <div className="container">
+          <div className="row">
+            <Switch>
+              <Route path="/" exact component={Login} />
+
+              <PrivateRoute
+                authed={state}
+                path="/dashboard"
+                component={DashBoard}
+              />
+              <Route render={() => <Login />} />
+            </Switch>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
